010d400a44b5bf222d6106a4d7c5b497c6ab67cf
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2022-01-10 20:58:33 +0800

    feat: add chart list

---
+++ src/components/ChartTypes.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Chart Types</h1>
+
+  <ul>
+    <li v-for="(item, index) in chartTypes" :key="index">
+      <a :href="`#${item}`">{{ item }}</a>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+const chartTypes = [
+  "line",
+  "bar",
+  "radar"
+  // "doughnut",
+  // "pie",
+  // "polar-area",
+  // "bubble",
+  // "scatter",
+  // "area",
+  // "mixed"
+]
+</script>
+
+<style scoped>
+ul {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+</style>
